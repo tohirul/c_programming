ab9f35a48bdb01e74e16241858860d76104ad9cf
tohirul <tohirul.islam164@gmail.com>
2024-12-12 18:30:44 +0600

    [Date] Dec 12, 2024 [Update] terary_operator.c [Complete] fuction to return max number using ternary operator

---
+++ ternary_operator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fid_max(int num1, int num2);
+
+int fid_max(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
+}
+
+int main() {
+    printf("Lets code some amazing things 🚀!!!\n\n");
+    // Your code here
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    printf("The maximum number is %d\n", fid_max(num1, num2));
+    return 0;
+}
