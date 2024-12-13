a41febcd8673b4e8029c975ce4493321f07047ea
tohirul <tohirul.islam164@gmail.com>
2024-12-13 15:04:17 +0600

    [Date] Dec 13, 2024 15:03 [Update] continue_break.c [Complete] Usecase of continue and break in loops

---
+++ continue_break.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("Lets code some amazing things 🚀!!!\n\n");
+    int x, iteration=0;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+
+    // Your code here
+    for (int i = 0; i < 10; i++) {
+
+        if (i == 5) {
+            continue;
+        }
+        if(i>x)
+            break;
+        iteration++;
+    }
+        printf("The loop completed %d iterations\n", iteration);
+    return 0;
+}
