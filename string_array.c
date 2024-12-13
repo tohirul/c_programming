23a7aafe5850f9db7b3688d147da395dc32a8fd3
tohirul <tohirul.islam164@gmail.com>
2024-12-13 16:57:26 +0600

    [Date] Dec 13, 2024 16:56 [Update] string_array.c [Complete] Array of strings looped through using size of array to determine array length

---
+++ string_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Lets code some amazing things 🚀!!!\n\n");
+    // Your code here
+    char cars[][10] = {"BMW", "Mercedes", "Toyota", "Tesla", "Ford", "Ferrari"};
+
+    for (int i = 0; i < sizeof(cars)/sizeof(cars[0]); i++) {
+        printf("%s\n", cars[i]);
+    }
+        return 0;
+}
